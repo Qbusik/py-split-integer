f9c33d7fb130a7e753fe8267147f4e9403065d41
Jakub Paluch <qbusik@gmail.com>
2025-11-17 09:34:32 +0100

    Solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,58 +1,21 @@
-import pytest
-
 from app.split_integer import split_integer
 
 
-@pytest.mark.parametrize(
-    "value,parts,result",
-    [
-        (8, 1, [8]),
-        (1, 4, [0, 0, 0, 1]),
-        (0, 5, [0, 0, 0, 0]),
-        (6, 2, [3, 3]),
-        (32, 6, [5, 5, 5, 5, 6, 6]),
-        (17, 4, [4, 4, 4, 5]),
-        (32, 6, [5, 5, 5, 5, 6, 6])
-    ]
-)
-def test_sum_of_the_parts_should_be_equal_to_value(
-        value: int,
-        parts: int,
-        result: list
-) -> None:
-    assert sum(split_integer(value, parts)) == sum(result)
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
+    assert sum(split_integer(17, 4)) == 17
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-        value: int,
-        parts: int,
-        result: list
-) -> None:
-    if value % parts == result:
-        assert all(split_integer(value, parts))
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert split_integer(60, 6) == [10, 10, 10, 10, 10, 10]
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part(
-        value: int,
-        parts: int,
-        result: list
-) -> None:
-    if parts == 1:
-        assert split_integer(value, parts) == result
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert split_integer(60, 1) == [60]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal(
-        value: int,
-        parts: int,
-        result: list
-) -> None:
-    assert sorted(split_integer(value, parts)) == result
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts(
-        value: int,
-        parts: int,
-        result: list
-) -> None:
-    if value < parts:
-        assert split_integer(value, parts) == result
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+    assert split_integer(2, 4) == [0, 0, 1, 1]
